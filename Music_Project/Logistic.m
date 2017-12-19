acc209172d4db75933df42c3faaf7a6029b296f6
JSerowik <JSerowik@protonmail.com>
2017-12-18 23:17:00 -0500

    Reorganized data and sorted by accuracy

--- Music_Project/Logistic.m
+++ Music_Project/Logistic.m
@@ -2,6 +2,5 @@ function Y = Logistic(X, md)
     sz = size(X);
     for i = 1:sz(1)
         Y(i,:) = 1 / (1 + exp(-0.1*(X(i,:)-md)));
-        %Y(i,:) = log(X(i,:)/(X(i,:)-1));
     end
 end
--- Music_Project/project.m
+++ Music_Project/project.m
@@ -1,17 +1,15 @@
 % Import tested solutions
 S1 = importdata('DataMatrix/result08377.txt');
-S2 = importdata('DataMatrix/result06942.txt');
-S3 = importdata('DataMatrix/result06927.txt');
-S4 = importdata('DataMatrix/result05483.txt');
-S5 = importdata('DataMatrix/result05499.txt');
-S6 = importdata('DataMatrix/result05011.txt');
-S7 = importdata('DataMatrix/result04947.txt');
-S8 = importdata('DataMatrix/result05003.txt');
-S9 = importdata('DataMatrix/result08019.txt');
-S10 = importdata('DataMatrix/result07615.txt');
-S11 = importdata('DataMatrix/result08255.txt');
-%S12 = correct(S12);
-
+S2 = importdata('DataMatrix/result08255.txt');
+S3 = importdata('DataMatrix/result08019.txt');
+S4 = importdata('DataMatrix/result07615.txt');
+S5 = importdata('DataMatrix/result06942.txt');
+S6 = importdata('DataMatrix/result06927.txt');
+S7 = importdata('DataMatrix/result05499.txt');
+S8 = importdata('DataMatrix/result05483.txt');
+S9 = importdata('DataMatrix/result05011.txt');
+S10 = importdata('DataMatrix/result05003.txt');
+S11 = importdata('DataMatrix/result04947.txt');
 S = [S1 S2 S3 S4 S5 S6 S7 S8 S9 S10 S11];
 % Convert each element by 2*x - 1
 t1 = convT(S1);
@@ -30,16 +28,16 @@ T = [t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11];
 temp = (T.')*T;
 % Input error rates returned from submission website
 err1 = (2*0.8377) - 1;
-err2 = (2*0.6942) - 1;
-err3 = (2*0.6927) - 1;
-err4 = (2*0.5483) - 1;
-err5 = (2*1.5499) - 1;
-err6 = (2*0.5011) - 1;
-err7 = (2*0.4947) - 1;
-err8 = (2*0.5003) - 1;
-err9 = (2*1.8019) - 1;
-err10 = (2*0.7615) - 1;
-err11 = (2*0.8255) - 1;
+err2 = (2*0.8255) - 1;
+err3 = (2*1.8019) - 1;
+err4 = (2*0.7615) - 1;
+err5 = (2*0.6942) - 1;
+err6 = (2*0.6927) - 1;
+err7 = (2*1.5499) - 1;
+err8 = (2*0.5483) - 1;
+err9 = (2*0.5011) - 1;
+err10 = (2*0.5003) - 1;
+err11 = (2*0.4947) - 1;
 errT = [err1; err2; err3; err4; err5; err6; err7; err8; err9; err10; err11];
 % ER = M*[2*correct rate - 1]
 ER = size(T, 1).*errT;
@@ -48,9 +46,8 @@ a = temp*ER;
 % S = S*a
 Sopt = S*a;
 md = mean(Sopt);
-[md]
 St = Logistic(Sopt, md);
 Sout = correct(St);
-fileID = fopen('DataMatrix/out.txt','w');
+fileID = fopen('DataMatrix/out1.txt','w');
 nbytes = fprintf(fileID,'%d\n',Sout);
 fclose(fileID);
